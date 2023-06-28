d323e164525694f879046f79c08e359c441bd91e
Mattias Törnqvist <mtoxr09@student.lnu.se>
2023-06-28 16:30:18 +0200

    microservices working through broker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @EventPattern('message_pattern')
+  async handleEvent(@Payload() data: string) {
+    console.log(data); // This will log the message sent from the other microservice
+  }
+
+  @MessagePattern('message_pattern')
+  async handleMessage(@Payload() data: string) {
+    console.log(data); // This will log the message sent from the other microservice
+    return 'Hello back from MS1';
+  }
 
   @Get()
   getHello(): string {
